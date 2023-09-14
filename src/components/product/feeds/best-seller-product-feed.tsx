be3c3f9c0b88a19f0cae810bea533ff4af23a3ab
RakibMiahMVN <rakib.miah@moveon.com.bd>
2023-09-14 11:19:15 +0600

    feat: home 1 done

--- src/components/product/feeds/best-seller-product-feed.tsx
+++ src/components/product/feeds/best-seller-product-feed.tsx
@@ -1,23 +1,27 @@
+import type { FC } from 'react';
 import ProductsCarousel from '@components/product/products-carousel';
-import { useBestSellerProductsQuery } from '@framework/product/get-all-best-seller-products';
 import { LIMITS } from '@framework/utils/limits';
+import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 import { ROUTES } from '@utils/routes';
 
-export default function BestSellerProductFeed() {
-  const { data, isLoading, error } = useBestSellerProductsQuery({
-    limit: LIMITS.BEST_SELLER_PRODUCTS_LIMITS,
-    id: 'pbs1',
-  });
+interface ProductFeedProps {
+  className?: string;
+  products: PricedProduct[]
+}
+
+const BestSellerProductFeed: FC<ProductFeedProps> = ({ className, products }) => {
   return (
     <ProductsCarousel
       sectionHeading="text-best-sellers"
       categorySlug={ROUTES.PRODUCTS}
-      products={data}
-      loading={isLoading}
-      error={error?.message}
+      products={products}
+      loading={false}
+      error={""}
       limit={LIMITS.BEST_SELLER_PRODUCTS_LIMITS}
       uniqueKey="best-sellers"
       className = "mb-8"
     />
   );
 }
+
+export default BestSellerProductFeed
--- src/components/product/feeds/suppercategory-cloth-feed.tsx
+++ src/components/product/feeds/suppercategory-cloth-feed.tsx
@@ -1,21 +1,23 @@
-import { LIMITS } from '@framework/utils/limits';
+import { FC } from 'react';
 import SupperCategoryList from "@components/suppercategory/suppercategory-list";
 import SupperCategoryContainer from "@components/suppercategory/suppercategory-container";
-import {useClothCategoryQuery} from "@framework/product/get-cloth-category";
-import {UsefashionProductsQuery} from "@framework/product/get-all-fashion-products";
 import {useRouter} from "next/router";
 import {getDirection} from "@utils/get-direction";
+import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 
-export default function SupperCategoryElectronicFeed() {
-  const { data: category } = useClothCategoryQuery({
-    limit: LIMITS.FASHION_PRODUCTS_LIMITS,
-  });
-  const { data: products, isLoading, error } = UsefashionProductsQuery({
-    limit: LIMITS.FASHION_PRODUCTS_LIMITS,
-  });
+interface ProductFeedProps {
+  className?: string;
+  products: PricedProduct[]
+}
+
+const SupperCategoryClothFeed: FC<ProductFeedProps> = ({ className, products }) => {
   const { locale } = useRouter();
   const dir = getDirection(locale);
   const backgroundThumbnail = dir === 'ltr' ? '/assets/images/collection/cate_2.jpg' : '/assets/images/collection/cate_2_rtl.jpg';
+
+  const category = {
+    name: "Clothes",
+  }
 
   return (
       <div className="mb-8 lg:mb-12">
@@ -26,9 +28,11 @@ export default function SupperCategoryElectronicFeed() {
             <SupperCategoryList className={`supper-category--list`} data={category}/>
           </div>
           <div className="trendy-main-content w-full p-2.5">
-            <SupperCategoryContainer data = {products} isLoading={isLoading} error={error}/>
+            <SupperCategoryContainer data={products} isLoading={false} error={false}/>
           </div>
         </div>
       </div>
   );
 }
+
+export default SupperCategoryClothFeed
